5f10ae73108f4438095ed98f21569da98c9bdafe
Pablo <pablo2003miguel0903@gmail.com>
2024-07-29 18:14:14 -0300

    finalizando seção de axios

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ console.log(axios);
 //2 - primeiro request
 const getData = async() => {
     try {
-        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        const response = await postsFetch.get("/users",
+            //4 - definindo headers
+            {
+                headers:{
+                    "Content-Type": "spplication/json",
+                    custom: "header",
+                },
+            },
+        );
+
+
         console.log(response);
         return response.data;
     } catch (error) {
@@ -12,3 +22,41 @@ const getData = async() => {
     }
 };
 getData();
+
+//3 - imprimindo dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async() =>{
+    const data = await getData();
+
+    console.log(data);
+
+    data.forEach((user) =>{
+        const div = document.createElement("div");
+        const nameElement = document.createElement("h3");
+        const emailElement = document.createElement("p");
+
+        nameElement.textContent = user.name;
+        emailElement.textContent = user.email;
+
+        div.appendChild(nameElement);
+        div.appendChild(emailElement);
+        container.appendChild(div);
+    })
+}
+printData();
+
+//5 - post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    postsFetch.post("/posts", {
+        title: titleInput.value,
+        body: bodyInput.value,
+        userId: 1,
+    });
+});
